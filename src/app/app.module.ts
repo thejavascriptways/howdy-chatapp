95c215800bf8d2691495d94c1361f6adbec17735
Deepak Singla <thejavascriptways@gmail.com>
2017-04-10 00:08:13 -0500

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+import { LoginPageComponent } from './login-page/login-page.component';
+import {RouterModule, Routes} from "@angular/router";
+import {AF} from "../providers/af";
+import { HomePageComponent } from './home-page/home-page.component';
+import {FormsModule} from "@angular/forms";
+import { RegistrationPageComponent } from './registration-page/registration-page.component';
+
+export const firebaseConfig = {
+
+    apiKey: "AIzaSyDMkhqM9Qc5YXEFMSsbccrhPz-mUdA1tt8",
+    authDomain: "howdy-chatapp-5c1a9.firebaseapp.com",
+    databaseURL: "https://howdy-chatapp-5c1a9.firebaseio.com",
+    projectId: "howdy-chatapp-5c1a9",
+    storageBucket: "howdy-chatapp-5c1a9.appspot.com",
+    messagingSenderId: "26795758109"
+  };
+
+
+const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'login', component: LoginPageComponent },
+  { path: 'register', component: RegistrationPageComponent}
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule
+    AngularFireModule.initializeApp(firebaseConfig),
+    RouterModule.forRoot(routes),
+    FormsModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [ AppComponent, LoginPageComponent, HomePageComponent, RegistrationPageComponent ],
+  bootstrap: [ AppComponent ],
+  providers: [AF]
 })
-export class AppModule { }
+export class AppModule {}
